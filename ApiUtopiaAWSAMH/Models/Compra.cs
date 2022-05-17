5185416c141a02c7bf4390f894f07320ae78bb91
alvaroyako <alvaroyako2000@hotmail.com>
2022-05-17 09:31:50 +0200

    Add project files.

---
+++ ApiUtopiaAWSAMH/Models/Compra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiUtopiaAWSAMH.Models
+{
+    [Table("COMPRA")]
+    public class Compra
+    {
+        [Key]
+        [Column("NOMBREJUEGO")]
+        public string Nombre { get; set; }
+
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+
+        [Column("IDCOMPRA")]
+        public int IdCompra { get; set; }
+    }
+}
---
+++ ApiUtopiaAWSAMH/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiUtopiaAWSAMH.Models
+{
+    [Table("USUARIOS")]
+    public class Usuario
+    {
+        [Key]
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+        [Column("PASSWORD")]
+        public byte[] Password { get; set; }
+
+        [Column("SALT")]
+        public string Salt { get; set; }
+
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+
+        [Column("ROL")]
+        public string Rol { get; set; }
+
+        [NotMapped]
+        public string PasswordString { get; set; }
+    }
+}
